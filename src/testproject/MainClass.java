9f07f4bf79dbebba729bc207a7ed1cf3b073ec35
jvsHoGent <johan.vanschoor@hogent.be>
2015-01-05 16:11:52 +0100

    run method

--- src/testproject/MainClass.java
+++ src/testproject/MainClass.java
@@ -2,6 +2,9 @@ package testproject;
 
 public class MainClass {
     public static void main(String[] args) {
+        new MainClass().run();
+    }
+    public void run(){
         System.out.println("Hello world!");
         System.out.println("Hello world everybody!");
         System.out.println("Het is 16u!");
