776ce33c8d1a77544ec8dd0399fe149095dde8de
sv964 <sonia.vandermeersch@hogent.be>
2015-01-05 15:57:12 +0100

    test Sonia

--- src/testproject/MainClass.java
+++ src/testproject/MainClass.java
@@ -3,5 +3,6 @@ package testproject;
 public class MainClass {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world everybody!");
     }
 }
