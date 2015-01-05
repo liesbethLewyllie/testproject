2ee0f026d12e185763b68cf4096a5c441d3964bd
liesbethLewyllie <liesbeth.lewyllie@hogent.be>
2015-01-05 15:58:18 +0100

    16u

--- src/testproject/MainClass.java
+++ src/testproject/MainClass.java
@@ -4,5 +4,6 @@ public class MainClass {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello world everybody!");
+        System.out.println("Het is 16u!");
     }
 }
